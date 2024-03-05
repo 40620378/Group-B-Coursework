bd1aa6f61c349d9f440faa436e39e06c318368fc
40588129-napier <40588129@live.napier.ac.uk>
2024-03-05 18:57:36 +0000

    Added group by

--- src/main/resources/peopleDistributionCountry.sql
+++ src/main/resources/peopleDistributionCountry.sql
@@ -1,7 +1,8 @@
-SELECT sum_cities.reportName, sum_cities.population AS totalPopulation, SUM(sum_cities.population - sum_cities.city_population) AS totalNotCity, sum_cities.city_population AS totalCity
+SELECT reportName, population AS totalPopulation, SUM(sum_cities.population - sum_cities.city_population) AS totalNotCity, city_population AS totalCity
 FROM (
     SELECT country.Code AS code, country.Name AS reportName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     LEFT JOIN city ON country.Code = city.CountryCode
     GROUP BY country.Code, country.Name, country.Population
-) AS sum_cities;
+) AS sum_cities
+GROUP BY reportName, population, city_population;
