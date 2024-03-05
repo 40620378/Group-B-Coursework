355517f17845bd81e3590c84b3a91f2d94806dfe
40588129-napier <40588129@live.napier.ac.uk>
2024-03-05 18:03:26 +0000

    restructured city population query

--- src/main/resources/peopleDistributionCountry.sql
+++ src/main/resources/peopleDistributionCountry.sql
@@ -1,10 +1,9 @@
-SELECT country.Name AS reportName, country.Population AS totalPopulation, SUM(country.Population - sum_cities.city_population) AS totalNotCity, sum_cities.city_population AS totalCity
+SELECT sum_cities.reportName, sum_cities.population AS totalPopulation, SUM(country.Population - sum_cities.city_population) AS totalNotCity, sum_cities.city_population AS totalCity
 FROM country
 INNER JOIN(
-    SELECT country.Code AS code, country.Name AS reportName, SUM(city.population) AS city_population
+    SELECT country.Code AS code, country.Name AS reportName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     INNER JOIN city ON country.Code = city.CountryCode
     GROUP BY country.Code
 ) AS sum_cities
-ON country.Code = sum_cities.code
-GROUP BY country.Name;
+ON country.Code = sum_cities.code;
