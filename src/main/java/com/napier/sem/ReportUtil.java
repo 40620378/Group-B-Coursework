6a1dff9ae5e14a6d1c6290264102e6612548c218
Paul <157486877+40620378@users.noreply.github.com>
2024-03-11 23:53:25 +0000

    sql added + test

--- src/main/java/com/napier/sem/ReportUtil.java
+++ src/main/java/com/napier/sem/ReportUtil.java
@@ -226,4 +226,32 @@ public class ReportUtil {
         return result;
     }
 
+    /**
+     * Generates a report for the cities in a continent sorted from largest population to smallest.
+     * @param connection the connection to the database
+     * @return
+     */
+    public static ArrayList<City> citiesContinentByPopulation(Connection connection, String continent){
+        ArrayList<City> result = new ArrayList<City>();
+        try {
+            String[] params = {continent};
+            ResultSet resultSet = SQLUtil.run(connection, "citiesContinentByPopulation.sql", params);
+            while (resultSet.next()) {
+                City city = new City();
+                city.country = resultSet.getString("Country");
+                city.population = resultSet.getString("Population");
+                city.name = resultSet.getString("Name");
+                city.district = resultSet.getString("District");
+                result.add(city);
+            }
+            resultSet.close();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+        }
+        return result;
+    }
+
+
 }
