d8ab7e3fc9c2f59396cf95aa5d2eb46f24715dae
Paul <157486877+40620378@users.noreply.github.com>
2024-03-12 00:21:23 +0000

    added district sql and java

--- src/main/java/com/napier/sem/ReportUtil.java
+++ src/main/java/com/napier/sem/ReportUtil.java
@@ -280,7 +280,7 @@ public class ReportUtil {
         return result;
     }
 
-        /**
+    /**
      * Generates a report for the cities in a country sorted from largest population to smallest.
      * @param connection the connection to the database
      * @return
@@ -290,6 +290,33 @@ public class ReportUtil {
         try {
             String[] params = {country};
             ResultSet resultSet = SQLUtil.run(connection, "citiesCountryByPopulation.sql", params);
+            while (resultSet.next()) {
+                City city = new City();
+                city.country = resultSet.getString("Country");
+                city.population = resultSet.getString("Population");
+                city.name = resultSet.getString("Name");
+                city.district = resultSet.getString("District");
+                result.add(city);
+            }
+            resultSet.close();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+        }
+        return result;
+    }
+
+    /**
+     * Generates a report for the cities in a country sorted from largest population to smallest.
+     * @param connection the connection to the database
+     * @return
+     */
+    public static ArrayList<City> citiesDistrictByPopulation(Connection connection, String district){
+        ArrayList<City> result = new ArrayList<City>();
+        try {
+            String[] params = {district};
+            ResultSet resultSet = SQLUtil.run(connection, "citiesDistrictByPopulation.sql", params);
             while (resultSet.next()) {
                 City city = new City();
                 city.country = resultSet.getString("Country");
