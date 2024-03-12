9ad85ad7bd06b3658066b86ac9fd8aaf30d3d683
LJ101001 <40619754@live.napier.ac.uk>
2024-03-12 08:51:38 +0000

    changed inner join sql files

--- src/main/resources/citiesRegionByPopulation.sql
+++ src/main/resources/citiesRegionByPopulation.sql
@@ -1,6 +1,6 @@
 SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country, city.District AS District
 FROM country
-INNER JOIN city ON country.Capital = city.ID
+INNER JOIN city ON country.Code = city.CountryCode
 WHERE country.Region = ?
 ORDER BY city.Population DESC
 
