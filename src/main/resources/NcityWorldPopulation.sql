45e71bd982fb71e51b03a6e43ecf4d3c32e629e8
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-17 21:11:03 +0000

    changed limit to 5 to double check it works

--- src/main/resources/NcityWorldPopulation.sql
+++ src/main/resources/NcityWorldPopulation.sql
@@ -2,4 +2,4 @@ SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country
 FROM country
 INNER JOIN city ON country.Code = city.CountryCode
 ORDER BY city.Population DESC
-LIMIT 10;
+LIMIT 5;
