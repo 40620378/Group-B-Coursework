b22eb2676b2126082147122e73b5c599e4aebfae
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-18 16:32:07 +0000

    set ? in NcityWorldPopulation.sql

--- src/main/resources/NcityWorldPopulation.sql
+++ src/main/resources/NcityWorldPopulation.sql
@@ -1,7 +1,5 @@
-SET @userInput = 12;
-
 SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country, city.District AS District
 FROM country
 INNER JOIN city ON country.Code = city.CountryCode
 ORDER BY city.Population DESC
-LIMIT @userInput;
+LIMIT ?;
