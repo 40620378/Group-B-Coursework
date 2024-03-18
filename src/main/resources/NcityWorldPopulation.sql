2e5d70046dbedaa46657846a60f7d6a171fe28f1
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-18 02:54:50 +0000

    changed variable n to userInput

--- src/main/resources/NcityWorldPopulation.sql
+++ src/main/resources/NcityWorldPopulation.sql
@@ -1,8 +1,8 @@
-DECLARE @N AS int 
-SET @N = 12
+DECLARE @userInput AS int 
+SET @userInput = 12
 
 SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country, city.District AS District
 FROM country
 INNER JOIN city ON country.Code = city.CountryCode
 ORDER BY city.Population DESC
-LIMIT @N;
+LIMIT @userInput;
