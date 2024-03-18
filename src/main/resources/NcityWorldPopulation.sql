168cb49b0faad57c2482010f74e012d2f15c02db
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-18 02:43:09 +0000

    using declare and set

--- src/main/resources/NcityWorldPopulation.sql
+++ src/main/resources/NcityWorldPopulation.sql
@@ -1,7 +1,8 @@
-DECLARE @N int AS CAST(? AS int)
+DECLARE @N AS int 
+SET @N = ?
 
 SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country, city.District AS District
 FROM country
 INNER JOIN city ON country.Code = city.CountryCode
 ORDER BY city.Population DESC
-LIMIT N;
+LIMIT @N;
