0f17b1b7b70fb0b5d1c8daca32df23841b11c537
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-18 03:00:16 +0000

    addded semicolon after declate

--- src/main/resources/NcityWorldPopulation.sql
+++ src/main/resources/NcityWorldPopulation.sql
@@ -1,4 +1,4 @@
-DECLARE @userInput AS int 
+DECLARE @userInput AS int;
 SET @userInput = 12
 
 SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country, city.District AS District
