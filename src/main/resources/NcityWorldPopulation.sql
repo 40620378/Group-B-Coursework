187bede09cbb9c3efb8243bcfb0fd6f5fbd554a6
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-18 02:10:10 +0000

    declaring local variable for N

--- src/main/resources/NcityWorldPopulation.sql
+++ src/main/resources/NcityWorldPopulation.sql
@@ -1,5 +1,7 @@
+DECLARE @N int = CAST(? AS int);
+
 SELECT city.Name AS Name, city.Population AS Population, country.Name AS Country, city.District AS District
 FROM country
 INNER JOIN city ON country.Code = city.CountryCode
 ORDER BY city.Population DESC
-LIMIT CAST(? AS int);
+LIMIT N;
