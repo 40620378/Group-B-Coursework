09efa6af75e424ac856e41ee9c99e3b9720206fe
40588129-napier <40588129@live.napier.ac.uk>
2024-03-19 11:39:10 +0000

    group query by region

--- src/main/resources/peopleDistributionRegion.sql
+++ src/main/resources/peopleDistributionRegion.sql
@@ -1,8 +1,9 @@
-SELECT reportName, population AS totalPopulation, SUM(sum_cities.population - sum_cities.city_population) AS totalNotCity, city_population AS totalCity
+SELECT country.Region AS Region, sum_cities.population AS totalPopulation, SUM(sum_cities.population - sum_cities.city_population) AS totalNotCity, sum_cities.city_population AS totalCity
 FROM (
-    SELECT country.Code AS code, country.Region AS reportName, country.Population AS population, SUM(city.population) AS city_population
+    SELECT country.Code AS code, country.Name AS countryName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     LEFT JOIN city ON country.Code = city.CountryCode
-    GROUP BY country.Code, country.Region, country.Population
+    GROUP BY country.Code, country.Name, country.Population
 ) AS sum_cities
-GROUP BY reportName, population, city_population;
+INNER JOIN country on sum_cities.code = country.Code
+GROUP BY country.Region, sum_cities.population, sum_cities.city_population;
