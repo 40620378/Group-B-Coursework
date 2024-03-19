b32b66e4e2f6b0c2214bdbc1ed6b74cbbdf332a5
Paul <157486877+40620378@users.noreply.github.com>
2024-03-19 11:17:05 +0000

    added queries

---
+++ src/main/resources/topNPopulatedCountriesRegion.sql
@@ -0,0 +1,6 @@
+SELECT country.Code AS code, country.Name AS Name, country.Continent AS continent, country.Region AS Region, country.Population AS Population, city.Name AS Capital
+FROM country
+INNER JOIN city ON country.Capital = city.ID
+WHERE country.Region = ?
+ORDER BY country.population DESC
+LIMIT ?;
