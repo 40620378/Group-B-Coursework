c1a54b3a2548db8a3cfe262158da0a6876fadd8c
Paul <157486877+40620378@users.noreply.github.com>
2024-03-19 12:06:04 +0000

    testing without join in query

--- src/main/resources/topNPopulatedCountriesWorld.sql
+++ src/main/resources/topNPopulatedCountriesWorld.sql
@@ -1,5 +1,4 @@
 SELECT country.Code AS code, country.Name AS Name, country.Continent AS continent, country.Region AS Region, country.Population AS Population, city.Name AS Capital
 FROM country
-INNER JOIN city ON country.Code = city.CountryCode
 ORDER BY country.population DESC
 LIMIT ?;
