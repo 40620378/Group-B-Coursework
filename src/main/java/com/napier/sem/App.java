8102197e018df4729abbc9cecf4d61bf2ff15f86
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-24 20:43:41 +0000

    checking N cities district works

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -19,7 +19,7 @@ public class App
         //get instance of SingletonConnection class
         SingletonConnection connectionManager = SingletonConnection.getInstance();
         Connection connection = connectionManager.connect();
-        ArrayList<City> result = ReportUtil.citiesDistrictByPopulation(connection, "England"); //, "4"
+        ArrayList<City> result = ReportUtil.nCitiesDistrictByPopulation(connection, "England", "4");
          if(result.size() > 0){
             for (int i = 0; i < result.size(); i++){
                 City Result = result.get(i);
