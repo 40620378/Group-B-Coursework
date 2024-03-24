a0a40561d099f6bb02b8f4591e21095601599376
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-24 21:01:13 +0000

    removed where clause

--- src/main/resources/nCapitalWorldByPopulation.sql
+++ src/main/resources/nCapitalWorldByPopulation.sql
@@ -1,6 +1,5 @@
 SELECT city.Name AS Capital, city.Population AS Population, country.Name AS Name
 FROM country
 INNER JOIN city ON country.Capital = city.ID
-WHERE country.? = ?
 ORDER BY city.Population DESC
 LIMIT ?
