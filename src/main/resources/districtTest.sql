8acd1389ceee1ced1e007e7ebbe50b03cfcdb49d
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-24 20:26:28 +0000

    added back inner join

--- src/main/resources/districtTest.sql
+++ src/main/resources/districtTest.sql
@@ -1,4 +1,5 @@
 SELECT city.Name AS Name, city.Population AS Population, city.District AS District
-FROM city
+FROM country
+INNER JOIN city ON country.Capital = city.ID
 WHERE city.? = ?
 ORDER BY city.Population DESC
