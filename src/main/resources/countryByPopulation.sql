2b9c3337677fa788c85cdc01c6cc41ed47486f79
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-29 11:36:14 +0000

    removed inner join

--- src/main/resources/countryByPopulation.sql
+++ src/main/resources/countryByPopulation.sql
@@ -1,5 +1,4 @@
 SELECT country.Name AS Name, country.Continent AS Continent, country.Population AS Population, country.Code AS Code, country.Region AS Region, city.Name AS Capital
 FROM country
-INNER JOIN city ON country.Code = city.CountryCode
 WHERE country.? = ?
 ORDER BY country.Population DESC;
