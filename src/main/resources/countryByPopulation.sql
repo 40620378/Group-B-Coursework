e767932247b7b19b4a22139fb8720c40faacbcec
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-03-29 11:40:36 +0000

    changed city. to country.

--- src/main/resources/countryByPopulation.sql
+++ src/main/resources/countryByPopulation.sql
@@ -1,4 +1,4 @@
-SELECT country.Name AS Name, country.Continent AS Continent, country.Population AS Population, country.Code AS Code, country.Region AS Region, city.Name AS Capital
+SELECT country.Name AS Name, country.Continent AS Continent, country.Population AS Population, country.Code AS Code, country.Region AS Region, country.Capital AS Capital
 FROM country
 WHERE country.? = ?
 ORDER BY country.Population DESC;
