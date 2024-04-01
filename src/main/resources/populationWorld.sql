3c091017b39443c5400f5ee7e415794fbedd913a
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-01 04:44:54 +0100

    test: trying simple query

--- src/main/resources/populationWorld.sql
+++ src/main/resources/populationWorld.sql
@@ -1,8 +1,2 @@
-SELECT population AS totalPopulation
-FROM (
-    SELECT country.Code AS code, country.Name AS reportName, country.Population AS population, SUM(city.population) AS city_population
-    FROM country
-    LEFT JOIN city ON country.Code = city.CountryCode
-    GROUP BY country.Code, country.Name, country.Population
-)
-GROUP BY population;
+SELECT SUM(population) AS totalPopulation
+FROM country;
