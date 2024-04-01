df9ebbbee5d0f688678c1802208802db523e48d6
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-01 04:40:07 +0100

    fix: adding from to sql

--- src/main/resources/populationWorld.sql
+++ src/main/resources/populationWorld.sql
@@ -1,2 +1,8 @@
 SELECT population AS totalPopulation
+FROM (
+    SELECT country.Code AS code, country.Name AS reportName, country.Population AS population, SUM(city.population) AS city_population
+    FROM country
+    LEFT JOIN city ON country.Code = city.CountryCode
+    GROUP BY country.Code, country.Name, country.Population
+)
 GROUP BY population;
