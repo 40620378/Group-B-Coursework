87495b55a0f0f2d953195350769c4c75e1745471
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-06 23:54:24 +0100

    test: peopleDistributionCountry

--- src/main/java/com/napier/sem/ReportUtil.java
+++ src/main/java/com/napier/sem/ReportUtil.java
@@ -14,29 +14,29 @@ public class ReportUtil {
      * @param connection the connection to the database
      * @return
      */
-    // public static ArrayList<Population> peopleDistributionCountry(Connection connection){
-    //     ArrayList<Population> result = new ArrayList<Population>();
-    //     try {
-    //         String[] params = {};
-    //         ResultSet resultSet = SQLUtil.run(connection, "peopleDistributionCountry.sql", params);
-    //         while (resultSet.next()) {
-    //             Population pop = new Population();
-    //             pop.reportName = resultSet.getString("reportName");
-    //             pop.totalCity = resultSet.getInt("totalCity");
-    //             pop.totalPopulation = BigInteger.valueOf(myInteger.intValue(resultSet.getInt("totalCity")));
-    //             pop.totalNotCity = resultSet.getInt("totalNotCity");
-    //             pop.percentageCity = roundedPercentage(pop.totalCity, pop.totalPopulation);
-    //             pop.percentageNotCity = roundedPercentage(pop.totalNotCity, pop.totalPopulation);
-    //             result.add(pop);
-    //         }
-    //         resultSet.close();
-    //     }
-    //     catch(Exception e) {
-    //         System.out.println(e.getMessage());
-    //         System.out.println("Failed to get population details");
-    //     }
-    //     return result;
-    // }
+    public static ArrayList<Population> peopleDistributionCountry(Connection connection){
+        ArrayList<Population> result = new ArrayList<Population>();
+        try {
+            String[] params = {};
+            ResultSet resultSet = SQLUtil.run(connection, "peopleDistributionCountry.sql", params);
+            while (resultSet.next()) {
+                Population pop = new Population();
+                pop.reportName = resultSet.getString("reportName");
+                pop.totalCity = resultSet.getInt("totalCity");
+                pop.totalPopulation = BigInteger.valueOf(myInteger.intValue(resultSet.getInt("totalCity")));
+                pop.totalNotCity = resultSet.getInt("totalNotCity");
+                pop.percentageCity = roundedPercentage(pop.totalCity, pop.totalPopulation);
+                pop.percentageNotCity = roundedPercentage(pop.totalNotCity, pop.totalPopulation);
+                result.add(pop);
+            }
+            resultSet.close();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+        }
+        return result;
+    }
 
     /**
      * Generates a report for the countries in a continent sorted from largest population to smallest.
