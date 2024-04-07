babbe31c4064a91e410efba9f0f5a05b22679167
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 00:25:14 +0100

    changed reportName from country to continent

--- src/main/resources/peopleDistribution.sql
+++ src/main/resources/peopleDistribution.sql
@@ -1,8 +1,8 @@
 SELECT Continent, population AS totalPopulation, SUM(sum_cities.population - sum_cities.city_population) AS totalNotCity, city_population AS totalCity
 FROM (
-    SELECT country.Code AS code, country.Name AS reportName, country.Population AS population, SUM(city.population) AS city_population
+    SELECT country.Code AS code, country.Continent AS reportName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     LEFT JOIN city ON country.Code = city.CountryCode
     GROUP BY country.Code, country.Name, country.Population
 ) AS sum_cities
-GROUP BY Continent, reportName, population, city_population;
+GROUP BY reportName, population, city_population;
