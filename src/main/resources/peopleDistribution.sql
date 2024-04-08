71fa19ed0ed98ec92080895dacc02bd8b505582a
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 17:19:03 +0100

    group by reportName in inner query

--- src/main/resources/peopleDistribution.sql
+++ src/main/resources/peopleDistribution.sql
@@ -3,6 +3,6 @@ FROM (
     SELECT country.Code AS code, country.Continent AS reportName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     LEFT JOIN city ON country.Code = city.CountryCode
-    GROUP BY country.Code, country.Name, country.Population
+    GROUP BY reportName, country.Code, country.Name, country.Population
 ) AS sum_cities
 GROUP BY reportName, population, city_population;
