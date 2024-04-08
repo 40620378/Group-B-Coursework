c13c72d1977fc8982d8494040b4b2a220a51927e
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 16:56:35 +0100

    removed country code from group by

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -2,5 +2,7 @@ SELECT Language
 FROM countrylanguage
 RIGHT JOIN city ON countrylanguage.CountryCode = city.CountryCode
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
-GROUP BY Language, CountryCode
+GROUP BY Language
 -- ORDER BY noOfSpeakers;
+-- IsOfficial, Percentage
+-- AS noOfSpeakers, As percentWorldPop
