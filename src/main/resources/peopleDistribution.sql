3c4ea36b25898bb225234ab47d24ac3f1c533250
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 17:28:20 +0100

    removed country.code from select

--- src/main/resources/peopleDistribution.sql
+++ src/main/resources/peopleDistribution.sql
@@ -1,6 +1,6 @@
 SELECT reportName, population AS totalPopulation, SUM(sum_cities.population - sum_cities.city_population) AS totalNotCity, city_population AS totalCity
 FROM (
-    SELECT country.Code AS code, country.Continent AS reportName, country.Population AS population, SUM(city.population) AS city_population
+    SELECT country.Continent AS reportName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     LEFT JOIN city ON country.Code = city.CountryCode
     GROUP BY reportName
