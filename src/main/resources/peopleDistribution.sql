d63e8b6a52f2330fe84e623a8fabc8171d0babd6
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 17:31:14 +0100

    added fields to internal group by

--- src/main/resources/peopleDistribution.sql
+++ src/main/resources/peopleDistribution.sql
@@ -3,6 +3,6 @@ FROM (
     SELECT country.Continent AS reportName, country.Population AS population, SUM(city.population) AS city_population
     FROM country
     LEFT JOIN city ON country.Code = city.CountryCode
-    GROUP BY reportName
+    GROUP BY reportName, population, city_population
 ) AS sum_cities
 GROUP BY reportName, population, city_population;
