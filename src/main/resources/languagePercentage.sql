c2fa7920ca66f9a36cb32d2d2ff4c0c3ceb989c9
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 15:06:03 +0100

    added CountryCode to group by

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -2,5 +2,5 @@ SELECT CountryCode, Language, IsOfficial, Percentage
 -- , AS noOfSpeakers, As percentWorldPop
 FROM countrylanguage
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
-GROUP BY Language
+GROUP BY Language, CountryCode
 -- ORDER BY noOfSpeakers;
