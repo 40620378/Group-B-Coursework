70d6be785a74586c7fd8e7c6b9a5ae2663f40d7c
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 16:50:39 +0100

    removed comments

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -1,6 +1,4 @@
 SELECT Language
---, IsOfficial, Percentage
--- , AS noOfSpeakers, As percentWorldPop
 FROM countrylanguage
 RIGHT JOIN city ON countrylanguage.CountryCode = city.CountryCode
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
