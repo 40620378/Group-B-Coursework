a37022aa841fc94e19e32d817faa575188b06312
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-08 15:03:36 +0100

    checking language is within list

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -1,6 +1,6 @@
 SELECT CountryCode, Language, IsOfficial, Percentage
 -- , AS noOfSpeakers, As percentWorldPop
 FROM countrylanguage
--- WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
--- GROUP BY Language
+WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
+GROUP BY Language
 -- ORDER BY noOfSpeakers;
