e141c0f9d30bed563d7457995baa1b6c59aaa483
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-09 15:02:57 +0100

    removed extra semi colon ;

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -2,5 +2,5 @@ SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS
 FROM countrylanguage
 INNER JOIN country ON countrylanguage.CountryCode = country.Code
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
-GROUP BY Language;
-ORDER BY SUM((country.Population / 100) * countrylanguage.Percentage) DESC;
+GROUP BY Language
+ORDER BY noOfSpeakers DESC;
