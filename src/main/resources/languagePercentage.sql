81b1a7db8eab8130875eef4626b2c973612d21f8
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-09 12:41:24 +0100

    added subquery for language population

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -1,4 +1,4 @@
-SELECT Language
+SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers
 FROM countrylanguage
 INNER JOIN country ON countrylanguage.CountryCode = country.Code
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
