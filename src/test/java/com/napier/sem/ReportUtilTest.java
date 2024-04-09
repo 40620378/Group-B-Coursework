17e7487f60a690635690831ec81ecbdd8d5d803b
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-09 15:05:32 +0100

    added languagePercentage unit test

--- src/test/java/com/napier/sem/ReportUtilTest.java
+++ src/test/java/com/napier/sem/ReportUtilTest.java
@@ -370,20 +370,20 @@ public class ReportUtilTest
         assertEquals(expected, resultList.get(0).ToString());
     }
 
-    // @Test
-    // void languagePercentage() throws SQLException {
-    //     Connection con = mock(Connection.class);
-    //     ResultSet result = mock(ResultSet.class);
-    //     Statement stmt = mock(Statement.class);
-    //     Mockito.when(con.createStatement()).thenReturn(stmt);
-    //     Mockito.when(stmt.executeQuery(anyString())).thenReturn(result);
-    //     Mockito.when(result.next()).thenReturn(true).thenReturn(false);
-    //     Mockito.when(result.getString("Language")).thenReturn("Chinese");
-    //     Mockito.when(result.getInt("noOfSpeakers")).thenReturn("Tokyo");
-    //     Mockito.when(result.getFloat("percentWorldPop")).thenReturn("JPN");
-    //     ArrayList<Population> resultList = ReportUtil.languagePercentage(con);
-    //     assertEquals(1, resultList.size());
-    //     String expected = "City: Tokyo Country: JPN District: Tokyo-to Population: 7980230";
-    //     assertEquals(expected, resultList.get(0).ToString());
-    // }
+    @Test
+    void languagePercentage() throws SQLException {
+        Connection con = mock(Connection.class);
+        ResultSet result = mock(ResultSet.class);
+        Statement stmt = mock(Statement.class);
+        Mockito.when(con.createStatement()).thenReturn(stmt);
+        Mockito.when(stmt.executeQuery(anyString())).thenReturn(result);
+        Mockito.when(result.next()).thenReturn(true).thenReturn(false);
+        Mockito.when(result.getString("Language")).thenReturn("Chinese");
+        Mockito.when(result.getInt("noOfSpeakers")).thenReturn("1191843539");
+        Mockito.when(result.getFloat("percentWorldPop")).thenReturn("60.55");
+        ArrayList<Language> resultList = ReportUtil.languagePercentage(con);
+        assertEquals(1, resultList.size());
+        String expected = "Language: Chinese noOfSpeakers: 1191843539 percentWorldPop: 60.55%";
+        assertEquals(expected, resultList.get(0).ToString());
+    }
 }
