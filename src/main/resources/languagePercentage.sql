0519c12573dc8fb09789f7bb1da1e5f9fe3e8318
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-12 04:10:11 +0100

    tst: with noOfSpeakers variable

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -1,6 +1,11 @@
-SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers, ROUND(((SUM((country.Population / 100) * countrylanguage.Percentage) / SUM(population)) * 100), 2) AS percentWorldPop
+SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers, ROUND(((noOfSpeakers / SUM(population)) * 100), 2) AS percentWorldPop
 FROM countrylanguage
 INNER JOIN country ON countrylanguage.CountryCode = country.Code
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
 GROUP BY Language
 ORDER BY noOfSpeakers DESC;
+
+
+
+
+
