fc142a9afd4858a58b1adfd3e3f29e8651100fd4
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-12 04:14:04 +0100

    fix: trying with correct brackets

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -1,4 +1,4 @@
-SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers, ROUND(((noOfSpeakers / SUM(population)) * 100), 2) AS percentWorldPop
+SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers, ROUND((((SUM((country.Population / 100) * countrylanguage.Percentage) / SUM(population)) * 100), 2) AS percentWorldPop
 FROM countrylanguage
 INNER JOIN country ON countrylanguage.CountryCode = country.Code
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
