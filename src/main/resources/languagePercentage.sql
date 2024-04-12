1009c774fe9eae0942abe5b25f575ebb1ec58f4c
SulaMcdonald <158058430+SulaMcdonald@users.noreply.github.com>
2024-04-12 04:31:35 +0100

    tst: division percentage

--- src/main/resources/languagePercentage.sql
+++ src/main/resources/languagePercentage.sql
@@ -1,4 +1,4 @@
-SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers, ROUND(((SUM((country.Population / 100) * countrylanguage.Percentage) / SUM(population)) * 100), 2) AS percentWorldPop
+SELECT Language, SUM((country.Population / 100) * countrylanguage.Percentage) AS noOfSpeakers, SUM((country.Population / 100) * countrylanguage.Percentage) / SUM(population) AS percentWorldPop
 FROM countrylanguage
 INNER JOIN country ON countrylanguage.CountryCode = country.Code
 WHERE Language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic')
@@ -9,3 +9,9 @@ ORDER BY noOfSpeakers DESC;
 
 
 
+
+
+
+
+-- ROUND(((SUM((country.Population / 100) * countrylanguage.Percentage) / SUM(population)) * 100), 2)
+-- ROUND(((noOfSpeakers) / SUM(population)) * 100), 2)
